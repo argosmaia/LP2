94970acd692654ae08529c32f96e016aace3ffd5
ARGOS MAIA <33440266+DarkHoax@users.noreply.github.com>
2022-12-14 11:27:29 -0300

    Create DataHora.java

---
+++ DataEHora/src/modelo/DataHora.java
@@ -0,0 +1,32 @@
+package modelo;
+
+public class DataHora {
+	private Data dt;
+	private Hora h;
+	
+	public DataHora() {
+		
+	}
+	
+	public DataHora(int d, int m, int a, int h, int min, int s) {
+		this.dt = new Data(d, m, a);
+		this.h = new Hora(h, min, s);
+	}
+
+	//OPÇÃO 1
+	public DataHora(int d, int m, int a) {
+		this.dt = new Data(d, m, a);
+		this.h = new Hora();
+	}
+	
+	//OPÇÃO 2
+	public DataHora(Data d, Hora h) {
+		this.dt = d;
+		this.h = h;
+	}
+
+	@Override
+	public String toString() {
+		return "Data "+dt+" Hora "+h;
+	}	
+}
